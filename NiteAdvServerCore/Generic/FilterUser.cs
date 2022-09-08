116291cba44840bf133e408fdd032fc7461ab84b
mac <marco.bazzoli@outlook.it>
2022-09-08 14:02:32 +0200

    prima versione claim locale

---
+++ NiteAdvServerCore/Generic/FilterUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NiteAdvServerCore.Generic;
+
+public class FilterCompany
+{
+    public FilterCompany()
+    {
+        Where = "";
+    }
+    public int IdCompany { get; set; }
+    public int PageSize { get; set; }
+    public int TotalItems { get; set; }
+    public int Offset { get; set; }
+    public string Where { get; set; }
+}
---
+++ NiteAdvServerCore/Generic/UserResponse.cs
@@ -0,0 +1,19 @@
+﻿using NiteAdvServerCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiteAdvServerCore.Generic
+{
+    public class CompanyResponse
+    {
+        public CompanyResponse()
+        {
+            CompanyList = new List<Company>();
+        }
+       public List<Company> CompanyList { get; set; }
+       public int ItemsCount { get; set; }
+    }
+}
